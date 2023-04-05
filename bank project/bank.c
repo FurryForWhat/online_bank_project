62d5f65990dbfa33a6fef486eb401c47fb49c7b6
furry33 <60417722+jokeryy5@users.noreply.github.com>
2023-04-06 00:23:03 +0630

    Update bank.c

--- bank project/bank.c
+++ bank project/bank.c
@@ -2,15 +2,25 @@
 // Created by ADMIN on 12-Feb-23.
 //
 #include "stdio.h"
-#include "online_bank.h"
+#include "../../bank project/online_bank.h"
 
 int main(){
 
      space_counter();
     loadingAllDataFromFile();
-    printingAllData();
-    welcome();
+//    printingAllData();
+//    welcome();
 
+
+//     get_time();
+//    printf("Current edited time: %s", Ctime[0].c_Time);
+//    integer_to_char(1000);
+
+    current_data_toTransfer(3000);
+//     calculate_amounts_same_days(0);
+    get_record_AandT(0);
+
+//    get_record_AandT(0);
     return 0;
 
 }
